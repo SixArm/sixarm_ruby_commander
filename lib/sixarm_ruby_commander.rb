d61302b0aac5054146fd1a26ec22c306de315076
SixArm <sixarm@sixarm.com>
2011-10-06 22:18:09 -0700

    Updates for gem publishing

--- lib/sixarm_ruby_commander.rb
+++ lib/sixarm_ruby_commander.rb
@@ -1,26 +1,6 @@
 # -*- coding: utf-8 -*-
-
 =begin rdoc
-
-= SixArm Ruby Gem: Commander is syntax sugar from Open4::spawn
-
-Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
-Copyright:: Copyright (c) 2006-2010 Joel Parker Henderson
-License:: CreativeCommons License, Non-commercial Share Alike
-License:: LGPL, GNU Lesser General Public License
-
-Kernel#commander sugar method wrapper for Open4::spawn
-
-==Syntax
-   commander(command, option=>value, option=>value, ...)
-
- ==Example
-   commander('ls')
-   => Open4::spawn('ls',:stdout=>'',:stderr=>'')
-   => 'ls', {}, status, stdout, stderr
-
-Any options will be sent along to the spawn method.
-
+Please see README.rdoc
 =end
 
 
--- sixarm_ruby_commander.gemspec
+++ sixarm_ruby_commander.gemspec
@@ -13,8 +13,10 @@ Gem::Specification.new do |s|
   s.platform          = Gem::Platform::RUBY
   s.require_path      = 'lib'
   s.has_rdoc          = true
-  s.files             = ['README.rdoc','LICENSE.txt','lib/sixarm_ruby_commander.rb']
-  s.test_files        = ['test/sixarm_ruby_commander_test.rb']
+
+  top_files           = [".gemtest", "CHANGELOG.txt", "INSTALL.txt", "LICENSE.txt", "Rakefile", "README.rdoc", "VERSION"]
+  lib_files           = ["lib/#{s.name}.rb"]
+  test_files          = ["test/#{s.name}_test.rb"]
 
   s.add_dependency('open4', '>=0.9.6')
 
